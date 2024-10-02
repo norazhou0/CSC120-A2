f80ed76d3e8fd2def045270dd6fd4ed4dc73bde9
Nora Zhou <nzhou@smith.edu>
2024-10-02 14:13:21 -0400

    revised

--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,5 +1,4 @@
 
-from typing import Dict, Optional
 from computer import *
 
 # create a class for resaleshop
@@ -14,35 +13,43 @@ class ResaleShop:
         self.inventory = inventory
 
     # create a function for buying computer
-    def buy_computer (self, c: Computer) -> None:
-        self.inventory.append(c)
+    def buy_computer (self, computer: Computer) -> None:
+        self.inventory.append(computer)
         print(self.inventory)
 
     # create a function for selling computer
-    def sell_computer (self, c: Computer) -> None:
-        if c in self.inventory:
-            self.inventory.remove(c)
-            print("Item", c.description, "sold!")
+    def sell_computer (self, computer: Computer) -> None:
+        if computer in self.inventory:
+            self.inventory.remove(computer)
+            print("Item", computer.description, "sold!")
         else: 
-            print("Item", c.description, "not found. Please select another item to sell.")
+            print("Item", computer.description, "not found. Please select another item to sell.")
 
     # crete a function for refurbishing computer
-    def refurbish(self, c: Computer, new_os: Optional[str] = None) -> None:
-        if c in self.inventory:
-            if int(c.year_made) < 2000:
-                c.price = 0 # too old to sell, donation only
-            elif int(c.year_made) < 2012:
-                c.price = 250 # heavily-discounted price on machines 10+ years old
-            elif int(c.year_made) < 2018:
-                c.price = 550 # discounted price on machines 4-to-10 year old machines
+    def refurbish(self, computer: Computer, new_os: str = None) -> None:
+        if computer in self.inventory:
+            if int(computer.year_made) < 2000:
+                computer.price = 0 # too old to sell, donation only
+            elif int(computer.year_made) < 2012:
+                computer.price = 250 # heavily-discounted price on machines 10+ years old
+            elif int(computer.year_made) < 2018:
+                computer.price = 550 # discounted price on machines 4-to-10 year old machines
             else:
-                c.price = 1000 # recent stuff
+                computer.price = 1000 # recent stuff
 
             if new_os is not None:
-                c.operating_system= new_os # update details after installing new OS
+                computer.operating_system= new_os # update details after installing new OS
         else:
-            print("Item", c.description, "not found. Please select another item to refurbish.")
-        
+            print("Item", computer.description, "not found. Please select another item to refurbish.")
+    
+    # create a method for printing inventory
+    def printInventory(self) -> None:
+        if not self.inventory:
+            print("The inventory is currently empty.")
+        else:
+            print("Inventory: ")
+            for computer in self.inventory:
+                print(computer.description)
 
 def main():
     # make a computer 
@@ -75,8 +82,11 @@ def main():
     print("There is", len(ResaleShop_1.inventory), "item in stock.")
     
     # refurbishing a computer
-    ResaleShop_1.refurbish(computer_1, "Windows 12")
+    ResaleShop_1.refurbish(computer_1, "Windows 12.")
     print("The operating system of",computer_1.description,"is updated to",computer_1.operating_system)
+
+    # print the inventory
+    ResaleShop_1.printInventory()
 
 if __name__ == "__main__":
     main()
